5598828b07f28ed8b4fc20e72bd4d149f8bf6583
Ravi Teja Gattu <ravitejagattu@Ravis-MacBook-Air.local>
2022-02-27 13:27:18 +0530

    UI5 view and controller

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-    res.send("Welcome to Book My Chair Application!");
+    res.send("Welcome to Book My Chair Application!!!");
 });
 
 app.listen(process.env.PORT || 8000);
