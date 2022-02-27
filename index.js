e652c77909c6e2878375c83d554f0cbe493d8026
Ravi Teja Gattu <ravitejagattu@Ravis-MacBook-Air.local>
2022-02-27 13:46:34 +0530

    installed mongodb

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-    res.send("Welcome to Book My Chair Application!!!");
-});
+
+const url = "mongodb+srv://ravitejagattu:GattuRavi@cluster0.zkqg1.mongodb.net/cluster0?retryWrites=true&w=majority";
+MongoClient.connect(url, { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db('UI5DB');
+        console.log("Connected to Database");
+        const masterRoleCollection = db.collection('masterrole');
+        // app.get('/', (req, res) => {
+        //     res.send("Welcome to Book My Chair Application!!!");
+        // })
+
+        //get method
+        app.get('/masterrole', (req, res) => {
+            db.collection('masterrole').find().toArray()
+                .then(results => {
+                    console.log(results);
+                    res.send(results);
+                })
+                .catch(error => console.error(error))
+        })
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
 
 app.listen(process.env.PORT || 8000);
