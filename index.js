19810110dc3e5b1c4148f1cbc2ad4c375aac0195
Ravi Teja Gattu <ravitejagattu@Ravis-MacBook-Air.local>
2022-02-27 13:53:46 +0530

    Post method added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 app.use(express.static(__dirname));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 const url = "mongodb+srv://ravitejagattu:GattuRavi@cluster0.zkqg1.mongodb.net/cluster0?retryWrites=true&w=majority";
 MongoClient.connect(url, { useUnifiedTopology: true })
     .then(client => {
@@ -13,6 +14,14 @@ MongoClient.connect(url, { useUnifiedTopology: true })
         //     res.send("Welcome to Book My Chair Application!!!");
         // })
 
+        //post method
+        app.post('/masterrolepost', (req, res) => {
+            masterRoleCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/masterrole')
+                })
+                .catch(error => { console.log(error) })
+        })
         //get method
         app.get('/masterrole', (req, res) => {
             db.collection('masterrole').find().toArray()
