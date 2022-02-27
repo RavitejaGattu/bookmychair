0e01aac932b2a93f82f651ea32222b22d7bef3f6
Ravi Teja Gattu <ravitejagattu@Ravis-MacBook-Air.local>
2022-02-27 13:25:48 +0530

    Welcome Message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-    res.send("Working");
+    res.send("Welcome to Book My Chair Application!");
 });
 
 app.listen(process.env.PORT || 8000);
